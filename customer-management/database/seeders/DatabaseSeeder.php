33625b810a8591e7cbac7490794557dda73a0829
Matheus Marques <matheusdosreis2004@gmail.com>
2026-08-22 21:48:34 -0300

    chore: bootstrap Laravel application

---
+++ customer-management/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Intentionally empty: uploaded photos should not be represented by fake paths.
+    }
+}
